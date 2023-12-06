2c573ae983a5a09b615171f090694735dd960ee3
Peter-The-Great <PjotrW15@Gmail.com>
2023-12-06 13:30:05 +0100

    Add project files.

---
+++ ConsoleApp1/Dependency.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp1;
+public interface IEngine
+{
+    void Start();
+}
+
+public class GasolineEngine : IEngine
+{
+    public void Start()
+    {
+        Console.WriteLine("Starting gasoline engine");
+    }
+}
+
+public class ElectricEngine : IEngine
+{
+    public void Start()
+    {
+        Console.WriteLine("Starting electric engine");
+    }
+}
+
+public class Car
+{
+    private IEngine engine;
+
+    public Car(IEngine engine)
+    {
+        this.engine = engine;
+    }
+
+    public void Start()
+    {
+        engine.Start();
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,80 @@
+﻿// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+namespace ConsoleApp1;
+using System;
+using System.Collections.Generic;
+
+//Extension Method Gewicht
+//Lijst van Dieren en een apartelijst van zoogdieren
+//krokodile
+
+/**
+ * Contra en covariance worden hier besproken
+ * 
+ * Specifiek hier is covariance.
+ */
+class Dier
+{
+    public string? Naam;
+    public int Gewicht = 0;
+}
+
+class Zoogdier : Dier { }
+
+class Reptiel : Dier { }
+
+class Krokodil : Reptiel { }
+
+/**
+ * * Hier wordt contravariance besproken
+ * */
+delegate void MyDelegate(Reptiel m);
+
+/*
+ * Dit is voor generics
+ */
+class Vogel<T, S>(T naam, S gewicht, S leeftijd)
+{
+    public T Naam { get; set; } = naam;
+    public S Gewicht { get; set; } = gewicht;
+    public S Leeftijd { get; set; } = leeftijd;
+
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //Methode1();
+    }
+
+    static void Methode1()
+    {
+        Vogel<string, int> vogel = new Vogel<string, int>("Piet", 10, 5);
+
+        List<Dier> dieren = new List<Dier>();
+        List<Zoogdier> zoogdieren = new List<Zoogdier>();
+        dieren.Add(new Zoogdier());
+        dieren.Add(new Krokodil());
+        zoogdieren.Add(new Zoogdier());
+        Console.WriteLine(dieren[0].Gewicht);
+        if (zoogdieren.Count > 0)
+        {
+            Console.WriteLine(zoogdieren[0].GetType());
+        }
+        Console.WriteLine("Vogel " + vogel.Naam + " is " + vogel.Leeftijd + " jaar oud.");
+
+        //Contravariance
+        MyDelegate del = new MyDelegate(Method);
+        del(new Krokodil());
+    }
+    static void Method(Reptiel a)
+    {
+        Console.WriteLine("Type van deze class is: " + a.GetType());
+    }
+    static void Methode2()
+    {
+        Car auto = new Car(new GasolineEngine());
+        auto.Start();
+    }
+}
