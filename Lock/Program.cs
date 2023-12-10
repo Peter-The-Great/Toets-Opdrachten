758192268bedfd973173e29a39475e5a42b651e8
Peter-The-Great <PjotrW15@Gmail.com>
2023-12-10 23:13:14 +0100

    Lock

---
+++ Lock/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static void Main()
+    {
+        var account = new Account(1000);
+
+        // Start 100 threads to update the account balance
+        var tasks = new Task[100];
+        for (int i = 0; i < tasks.Length; i++)
+        {
+            tasks[i] = Task.Run(() => Update(account));
+        }
+
+        // Wait for all threads to complete
+        Task.WaitAll(tasks);
+
+        //balance should be 2000
+        Console.WriteLine($"Account balance is {account.GetBalance()}");
+    }
+
+    public static void Update(Account account)
+    {
+        decimal[] amounts = { 0, 2, -3, 6, -2, -1, 8, -5, 11, -6 };
+
+        foreach (var amount in amounts)
+        {
+            if (amount >= 0)
+            {
+                account.Credit(amount);
+            }
+            else
+            {
+                account.Debit(Math.Abs(amount));
+            }
+        }
+    }
+}
+
+public class Account
+{
+    private readonly object balanceLock = new object();
+    private decimal balance;
+
+    public Account(decimal initialBalance)
+    {
+        balance = initialBalance;
+    }
+
+    public decimal Debit(decimal amount)
+    {
+        if (amount < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount), "Debit amount cannot be negative.");
+        }
+
+        decimal appliedAmount = 0;
+
+        lock (balanceLock)
+        {
+            if (balance >= amount)
+            {
+                balance -= amount;
+                appliedAmount = amount;
+            }
+        }
+
+        return appliedAmount;
+    }
+
+    public void Credit(decimal amount)
+    {
+        if (amount < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount), "Credit amount cannot be negative.");
+        }
+
+        lock (balanceLock)
+        {
+            balance += amount;
+        }
+    }
+
+    public decimal GetBalance()
+    {
+        lock (balanceLock)
+        {
+            return balance;
+        }
+    }
+}
